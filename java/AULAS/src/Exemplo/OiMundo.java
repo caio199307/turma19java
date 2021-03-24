19226cc69ef895a42af0e3ff8878e461f3d706c3
Caio Vitor <caio199307@gmail.com>
2021-03-24 16:45:25 -0300

    adicionando java

---
+++ java/AULAS/src/Exemplo/OiMundo.java
@@ -0,0 +1,10 @@
+package Exemplo;
+
+public class OiMundo {
+	public static void main(String[] args)
+	{
+		System.out.println("Oi Mundo.\nCaio Vitor");
+		System.out.println("Vamos programar em Java.");
+	}
+		
+}
