c7c843abe6e73889dc98fa387e75419899c24acb
Caio Vitor <caio199307@gmail.com>
2021-03-25 19:04:06 -0300

    correção do exercício de média ponderada.

--- java/AULAS/src/Exemplo/MediaAluno.java
+++ java/AULAS/src/Exemplo/MediaAluno.java
@@ -6,17 +6,17 @@ public class MediaAluno {
 	
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		int nota1,nota2,nota3,media;
+		double nota1,nota2,nota3,media;
 		
 		System.out.println("Digite a primeira nota: ");
-		nota1 = leia.nextInt();
+		nota1 = leia.nextDouble();
 		System.out.println("Digite a segunda: ");
-		nota2 = leia.nextInt();
+		nota2 = leia.nextDouble();
 		System.out.println("Digite a terceira nota: ");
-		nota3 = leia.nextInt();
-		media = (nota1+nota2+nota3)/3;
+		nota3 = leia.nextDouble();
+		media = ((2*nota1)+(3*nota2)+(5*nota3))/10;
 		
-		System.out.println("sua média é de: "+media);
+		System.out.printf("sua média é de: %.2f",media);
 		
 	}
 }
