7a4a8bcd5ce7eaf7bb7386f3da88d107a34ff755
Caio Vitor <caio199307@gmail.com>
2021-03-25 18:51:20 -0300

    adicionando exercício raiz das potências, é nóis, tmj

---
+++ java/AULAS/src/Exemplo/DistanciaRaiz.java
@@ -0,0 +1,35 @@
+package Exemplo;
+
+import java.util.Scanner;
+
+public class DistanciaRaiz {
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		double x1,x2,y1,y2,P1,P2,D;
+		
+		System.out.println("Valor de x1");
+		x1 = leia.nextDouble();
+		System.out.println("Valor de x2");
+		x2 = leia.nextDouble();
+		System.out.println("Valor de y1");
+		y1 = leia.nextDouble();
+		System.out.println("Valor de y2");
+		y2 = leia.nextDouble();
+		
+		P1 = x1-x2;
+		P1 = Math.pow(P1, 2);
+		
+		P2 = y1-y2;
+		P2 = Math.pow(P2, 2);
+		
+		D = Math.pow(P1 + P2, 0.5);
+		
+		System.out.printf("primeira equação: "+P1+" segunda equação: "+P2+" e a raiz quadrada é: %.2f",D);
+		
+		
+		
+		
+	}
+
+}
--- java/AULAS/src/Exemplo/ValorCarro.java
+++ java/AULAS/src/Exemplo/ValorCarro.java
@@ -1,20 +1,16 @@
 package Exemplo;
-
 import java.util.Scanner;
-
 public class ValorCarro {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		double distribuicao,imposto,custoFabrica,custoConsumidor;
-		
 		System.out.println("Digite o custo de fábrica do veículo ");
 		custoFabrica = leia.nextDouble();
 		distribuicao = custoFabrica*0.28;
 		imposto = custoFabrica*0.45;
 		custoConsumidor = custoFabrica+distribuicao+imposto;
-		System.out.println("O custo do veículo ao consumidor será de: "+custoConsumidor);
-			
+		System.out.println("O custo do veículo ao consumidor será de: "+custoConsumidor);	
 		}
-	}
+}
 
 
