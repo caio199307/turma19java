00125e2f74eb896225dd35a89eb6db5d4c56abbd
Caio Vitor <caio199307@gmail.com>
2021-04-06 13:57:37 -0300

    Exercício 6 adicionado.

--- java/AULAS/src/ListaLacoRepeticao/Exercicio6.java
+++ java/AULAS/src/ListaLacoRepeticao/Exercicio6.java
@@ -1,5 +1,30 @@
 package ListaLacoRepeticao;
 
-public class Exercicio6 {
+import java.util.Scanner;
 
+public class Exercicio6 {
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		double num1=0;
+		double media;
+		double contador=0;
+		double multiplosParaSomar=0;
+		do
+		{
+			System.out.println("Digite um número");
+			num1 = leia.nextDouble();
+			
+			if(num1 % 3 == 0)
+			{
+				multiplosParaSomar += num1;
+				contador++;
+			}
+			
+			
+		}while(num1 != 0);
+		media = multiplosParaSomar / contador;
+		System.out.printf("\nDos números digitador %.2f eram multiplos de 3\n",contador);
+		System.out.printf("A média dos números digitados que são multiplos de 3 é de %.2f",media);
+	}
 }
