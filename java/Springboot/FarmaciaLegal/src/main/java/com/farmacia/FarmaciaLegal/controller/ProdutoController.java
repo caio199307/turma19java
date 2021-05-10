6facdf069647fc5b8cf1aa7b7ae63add0f08d0aa
Caio Vitor <caio199307@gmail.com>
2021-05-10 08:17:58 -0300

    Adicionando camada security ao blog pessoal

--- java/Springboot/FarmaciaLegal/src/main/java/com/farmacia/FarmaciaLegal/controller/ProdutoController.java
+++ java/Springboot/FarmaciaLegal/src/main/java/com/farmacia/FarmaciaLegal/controller/ProdutoController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.farmacia.FarmaciaLegal.model.Categoria;
 import com.farmacia.FarmaciaLegal.model.Produto;
 import com.farmacia.FarmaciaLegal.repository.ProdutoRepository;
 
